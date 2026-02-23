467403420e0113fc419c8cfa1fc6839e948136b1
azka <fadlian.dev@gmail.com>
2026-02-23 09:51:37 +0700

    fix: update environment configuration and improve log level handling
    - Standardize environment variable values to uppercase (APP_ENV, LOG_LEVEL)
    - Add missing database connection variables (DB_HOST, DB_PORT)
    - Fix APP_PORT format by removing leading colon
    - Enhance logger to support multiple log levels (DEBUG, WARN, ERROR, INFO)
    - Ensure case-insensitive log level parsing

--- logger.go
+++ logger.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/log"
 )
 
 func init() {
-	if os.Getenv("LOG_LEVEL") == "debug" {
+	switch strings.ToUpper(os.Getenv("LOG_LEVEL")) {
+	case "DEBUG":
 		log.SetLevel(log.DebugLevel)
+	case "WARN":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
 	}
 }
